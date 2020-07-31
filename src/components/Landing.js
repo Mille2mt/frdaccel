705f93d1aed44b20d8148401ebe80245904a98c8
Matthew Miller <miller2mt@gmail.com>
2020-07-31 14:13:35 -0400

    hero complete

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 const Landing = () => (
     <div className="hero">
-        <div className="header row">
+        <div className="header fade-in">
             <h3 className='brandLogo'>FRD Accel</h3>
             <div className="menu">
-            <a href="/" className="nav-link">Us</a>
-            <a href="/" className="nav-link">About</a>
+                <a href="/" className="nav-link">Us</a>
+                <a href="/" className="nav-link">About</a>
             </div>
         </div>
     
         <div className="hero-body">
-            <h1 className='hero-content'>Further developing <span className='neon'>innovative</span> medical technologies</h1>
+            <h1 className='hero-content'><span className='fade-in'>Further developing </span><br/><span className='neon'>innovative</span><br/><span className='fade-in'> medical technologies</span></h1>
+        </div>
+
+        <div className='arrowDown'>
+            <span className="caron">&#9661;</span>
         </div>
     </div>
 )
