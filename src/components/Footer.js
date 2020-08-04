b03cf945069bf94aa00fc96180b56d98ec6c6727
Matthew Miller <miller2mt@gmail.com>
2020-08-04 10:34:22 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,14 @@ const Footer = () => (
     <div class="footer">
         <div class="container">
             <div class="row">
-                <h3 className='brandLogo-footer six columns'>FRD <span className='brandTitle'>Accel</span></h3>
-                <p class="copyright six columns">&copy; 2020</p>
+                <div className='four columns'>
+                    <h3 className='brandLogo-footer'>FRD <span className='brandTitle'>Accel</span></h3>
+                    <span className='copyright'>&copy; 2020</span>
+                </div>
+                <ul className='footer-list four columns'>
+                    <li>Charleston, South Carolina</li>
+                </ul>
+                <button className='footer-button four columns'>Contact us</button>                
             </div>
         </div>
     </div>
