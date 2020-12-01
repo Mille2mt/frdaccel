a77f74950d5efc6d31d9020ca9b6c5662a902eb8
Matthew Miller <miller2mt@gmail.com>
2020-12-01 12:59:11 -0500

    background image changed and menu color

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -11,25 +11,19 @@ const Stats = () => (
                 <div className="stats-content">    
 
                     <LazyLoad offset={-100} height={225}> 
-                        <div className="cardOne card four columns animated fadeInLeft">                            
+                        <div className="cardOne card six columns animated fadeInLeft">                            
                             <h2 className="number ">sub-header placeholder</h2>
                             <p className="subtitle">Text 1</p>                    
                         </div>  
                     </LazyLoad>
 
                     <LazyLoad offset={-250} height={250}>
-                        <div className="cardTwo card four columns animated fadeInLeft">
+                        <div className="cardTwo card six columns animated fadeInLeft">
                             <h2 className="number ">sub-header placeholder</h2>
                             <p className="subtitle">Text 2</p>
                         </div>
                     </LazyLoad>
-
-                    <LazyLoad offset={-350} height={250}>
-                        <div className="cardThree card four columns animated fadeInLeft">
-                            <h2 className="number ">sub-header placeholder</h2>
-                            <p className="subtitle">Text 3</p>
-                        </div>
-                    </LazyLoad>
+                    
 
                 </div>
             </div>
@@ -38,3 +32,11 @@ const Stats = () => (
 )
 
 export default Stats;
+
+// Optional card #3 - update column width to four if used
+// <LazyLoad offset={-350} height={250}>
+//     <div className="cardThree card four columns animated fadeInLeft">
+//         <h2 className="number ">sub-header placeholder</h2>
+//         <p className="subtitle">Text 3</p>
+//     </div>
+// </LazyLoad>
